f26ce3e6c19628dbfa062db7dbf751078bc0a1f4
joh33-wmko <jhayashi@keck.hawaii.edu>
2024-01-17 17:58:02 -1000

    identifies PIs and SAs who require IPAC access

--- verifyDataAccess.py
+++ verifyDataAccess.py
@@ -1,5 +1,7 @@
 # KOA Data Access Automation
-# runs daily via cron
+# run via cron on desired date for next month
+# runs daily via cron - TBD
+# run with kpython3 for logger functionality
 
 # Verification Types (vtype) to be processed
 # - PI                : PI Verification
@@ -8,16 +10,23 @@
 # - TDA (ToO or TWI)  : ToO/Twilight Program Verification
 # - KPF               : KPF Program Verification (SEM 2024B Aug 1, 2024)
 
+# ToDo:
+# - Observers needs to be changed from a list to a set (can change per semid for multiday)
+
+import pdb
+#pdb.set_trace()
+
 import argparse
+import calendar as cal
+from datetime import datetime as dt, timedelta
 import json
 import requests
-import datetime as dt
 import sys
 import urllib3
 urllib3.disable_warnings()
 
 # logger imports
-from LoggerClient import Logger as dl
+#from LoggerClient import Logger as dl
 import os
 import logging
 from logging import StreamHandler, FileHandler
@@ -36,17 +45,21 @@ def create_logger(subsystem, configLoc, author, progid, semid, fileName, loggern
     logger.setLevel(logging.INFO)
     return logger
 
-tclogger = create_logger('Data Access Automation', None, 'JPH', None, None, None, 'koa')
-tclogger.info('Running KOA Data Access Automation')
+#tclogger = create_logger('Data Access Automation', None, 'JPH', None, None, None, 'koa')
+#tclogger.info('Running KOA Data Access Automation')
 
 
 # APIs (move to config.live.ini)
 
 # WMKO Employee API
-emp_url = "https://www3build.keck.hawaii.edu/api/employee"
+#emp_url = "https://www3build.keck.hawaii.edu/api/employee"
+# https://vm-appserver.keck.hawaii.edu/api/employee/getEmployee?role=SA
+emp_url = "https://vm-appserver.keck.hawaii.edu/api/employee"
 
 # WMKO Telescope Schedule API
-sched_url = "https://www3build.keck.hawaii.edu/api/schedule"
+#sched_url = "https://www3build.keck.hawaii.edu/api/schedule"
+# https://vm-appserver.keck.hawaii.edu/api/schedule/getSchedule?date=2024-01-17
+sched_url = "https://vm-appserver.keck.hawaii.edu/api/schedule"
 
 # IPAC Access API (for GET_USERS_WITH_ACCESS and GET_SEMIDS_PER_USER)
 ipac_url = "http://vmkoatest.ipac.caltech.edu:8001/cgi-bin/PIAccess/nph-PIAccess_Auth.py"
@@ -99,58 +112,45 @@ for sa_item in sa_data:
     sa_info["firstname"] = sa_firstname
     sa_info["lastname"] = sa_lastname
     sa_info["email"] = sa_email
+    sa_info["alias"] = sa_alias
     sa_info["eid"] = sa_eid
     sa_obj[sa_alias] = sa_info
 
-#for k,v in sa_obj.items():
-#    print(k,v)
-
-#for x in sa:
-#    print(x, sa_obj[x])
-
-#print(f'SA Obj: {sa_obj}')
-
-print(f'SAs: {sa}\n')    
+#print(f'SAs: {sa}\n')    
+#print(f'SA Object: {sa_obj}\n')    
 
 # ====================
 
 required = admin + list(sa)
 
 # Type: PI Verification - send report monthly
-if vtype == 'PI':
-    # process argument(s) - move to main()
+#if vtype == 'PI':
 
-    # prepare vars
-    #startDate = dt.datetime.strptime(startDate, "%Y-%m-%d")
-    #endDate   = dt.datetime.strptime(endDate, "%Y-%m-%d")
-    #delta     = endDate - startDate
-    #days      = delta.days + 1
+#today = dt.now()
+#next_month = today.month + 1
+#this_year = today.year
+#num_days = cal.monthrange(this_year, next_month)[1]
+#startDate = f'{this_year}-{next_month}-1'
+#startDate = dt.strptime(startDate, '%Y-%m-%d')
+#endDate = startDate + timedelta(days=num_days-1)
 
-    # test vars
-    startDate = dt.datetime.strptime("2024-01-01", "%Y-%m-%d")
-    endDate   = dt.datetime.strptime("2024-01-31", "%Y-%m-%d")
-    #num_days = (endDate - startDate) + dt.timedelta(days=1)
-    #num_days = 31
-    num_days = 10
+#startDate = dt.strftime(startDate, '%Y-%m-%d')
+#endDate   = dt.strftime(endDate, '%Y-%m-%d')
 
-    print(f'startDate: {startDate}, endDate: {endDate}, numDays: {num_days}')
+# TEST DATES
+startDate = '2024-01-01'
+endDate = '2024-01-31'
+#num_days = 31
+num_days = 5
+# END TEST DATES
 
-    # prepare report date Monday prior to the start of next month, at least 7 days
-    # today = 
-    # month = month + 1
-    # last_dom =  # 28, 29, 30, or 31
-    # report_date = last_dom - 7 days
-    # report_date - 1 day until dat is Monday
-    # startDate = reportDate
-    # num_dim = ...
-    # endDate = startDate + num_dim
- 
-    url = sched_url
-    params = {}
-    params["cmd"]     = "getSchedule"
-    #startDate = "2024-01-03"
-    params["date"]    = startDate.strftime("%Y-%m-%d")
-    params["numdays"] = num_days
+print(f'startDate: {startDate}, endDate: {endDate}, numDays: {num_days}')
+
+url = sched_url
+params = {}
+params["cmd"]     = "getSchedule"
+params["date"]    = startDate
+params["numdays"] = num_days
 
 print(f'sched PARAMS (PIs for SEMIDs): {params}\n')
 
@@ -161,8 +161,6 @@ if not resp:
 else:
     data = resp.json()
 
-# process data for vtype
-
 # Type: PI Verification
 # - Grab all unique programs for the upcoming month using the telescope schedule API 
 
@@ -171,25 +169,30 @@ for entry in data:
     prog_code = f"{entry['Semester']}_{entry['ProjCode']}"
     prog_codes.add(prog_code)
 
-    # generate PI and Observers lists per prog_code
-    pi_alias = (entry['PiEmail'].split('@'))[0]
+    # generate PIs per prog_code
+    if entry['PiEmail']:
+        pi_alias = (entry['PiEmail'].split('@'))[0]
+    else:
+        pi_alias = entry['PiEmail']
+
     pi[prog_code] = f"{entry['PiEmail']}, {entry['PiLastName']}, {entry['PiFirstName']}, {pi_alias}"
 
-    # original - verify, may have been modified...
-    #if entry["ProjCode"] not in observers.keys():         # causes replicated observers ???
-        #observers[semid] = []
-    #observers[prog_code] += entry["Observers"].split(",")
-
-    if prog_code not in observers.keys():                 # fixes replicated observers ???
-        observers[prog_code] = []
+    # generate Observers per prog_code
+    if prog_code not in observers.keys():
+        observers[prog_code] = []           # add new prog_code list
     if 'Observers' in entry.keys():
-        #observers[prog_code] += entry["Observers"].split(",")
-        observers[prog_code] = entry["Observers"].split(",")
+        if entry["Observers"] != None:
+            #observers[prog_code] += entry["Observers"].split(",")
+            observers[prog_code] = entry["Observers"].split(",")   # fixes replicated observers
+            observers[prog_code] = set(observers[prog_code])
+        else:
+            observers[prog_code] = None 
 
 print()
-print(f'SEMIDS: {prog_codes}\n')      # list of prog_codes
-print(f'PIs: {pi}\n')
-print(f'OBSERVERS: {observers}\n')    # list of observers per prog_code
+#print(f'SEMIDS: {prog_codes}\n')      # list of prog_codes
+#print(f'PIs: {pi}\n')
+#print(f'SAs: {sa_obj}\n')    
+#print(f'OBSERVERS: {observers}\n')    # list of observers per prog_code
 
 print("***** WMKO API: PI and Observers *****\n")
 
@@ -197,15 +200,23 @@ for prog_code in prog_codes:
     print(f'{prog_code}: {pi[prog_code]}, {observers[prog_code]}')   # wmko everything
 print(f'{len(prog_codes)} prog_codes\n')
 
-print(f'PIs: {pi}')
+#print(f'SAs: {sa_obj}')
+print(f'SAs ({len(sa_obj)}):')
+for k,v in sa_obj.items():
+    print(f'{k}: {v}')
+print()
+
+#print(f'PIs: {pi}')
+print(f'PIs ({len(pi)}):')
 for k,v in pi.items():
     print(f'{k}: {v}')
-print(f'{len(pi)} PIs\n')
+print()
 
-print(f'OBSERVERS" {observers}')
+#print(f'OBSERVERS: {observers}')
+print(f'Observers ({len(observers)}):')
 for k,v in observers.items():
     print(f'{k}: {v}')
-print(f'{len(observers)} observers\n')
+print()
 
 # ============
 
@@ -217,28 +228,74 @@ params = {}
 params["request"] = "GET_USERS_WITH_ACCESS"
 
 for prog_code in prog_codes:
-    print(prog_code)
-    print(f" -- PI: {pi[prog_code]}")
-    print(f" -- Observers: {observers[prog_code]}")
-    print(f'PROG_CODE {prog_code}')
+    print(f'SEMID: {prog_code}')
+    #print(f" -- WMKO PI       : {pi[prog_code]}")
+    #print(f" -- WMKO SAs      : {sa}")
+    #print(f" -- WMKO Observers: {observers[prog_code]}")
+
     params["semid"] = prog_code
     resp = requests.get(url, params=params, auth=("KOA","Humu3"))   # use config.live.ini
     resp = resp.json()
 
-    #print(f' -- SAs:')
-    #for user in resp["response"]["detail"]:
-    if not resp["response"]["detail"]:
-        # ERROR
-        sys.exit()
+    ipac_users = set()
     for user in resp["response"]["detail"]:
         #print(user["userid"], user["email"], user["keckid"], user["first"], user["last"])
-        if user["userid"] in required:
-            print(f'+       {user}')
-        elif user["userid"] in ignore:
-            print(f'o       {user}')
-        else:
-            print(f'-       {user}')
+        #ipac_users.add(user["userid"])  # additional check in case email address is changed
+        ipac_users.add(user["email"].split("@")[0])
 
+    print(f' -- IPAC Users    : {ipac_users}')
+
+    print(f" -- WMKO PI       : {pi[prog_code]}")
+    wmko_pi_alias = pi[prog_code].split(",")[-1].strip()
+
+    #if wmko_pi_alias in ipac_users:
+    if wmko_pi_alias not in ipac_users:
+        print(f'    Access Required for PI: {pi[prog_code]}')
+
+    print(f" -- WMKO SAs      : {sa}")
+    for a_sa in sa:
+        if a_sa not in ipac_users:
+            print(f'    Access Required for SA: {sa_obj[a_sa]}')
+
+#    print(f" -- WMKO Observers: {observers[prog_code]}")
+
+#        # need to account for what is in WMKO and not in NExScI
+#
+#        # per SEMID (prog-code)
+#        if vtype == 'PI':
+#            # print if PI info not in NExScI info
+#            #print(f'PI\'s Alias: {pi[prog_code].split(",")[-1]}')
+#            print(f'USERID: {user["userid"]}')
+#
+#            if user['userid'] == pi[prog_code].split(",")[-1]:
+#                print(f'PI Match {user}')
+#            if user['userid'] in sa:
+#                print(f'SA Match {user}')
+#            
+#        if vtype == 'PI_OPS':
+#            # print if OPS info not in NExScI
+#            if user['userid'] == pi[prog_code]:
+#                print('PI Match {user}')
+#            if user['userid'] == sa[prog_code]:
+#                print('SA Match {user}')
+#            if user['userid'] == sa[prog_code]:
+#                print('OPS Match {user}')
+#        if vtype == 'COI_OBS':
+#            # process COI_OBS
+#            pass
+#        if vtype == 'TDA':
+#            # process TDA - ToO or TWI
+#            pass
+#        if vtype == 'KPF':
+#            # process KPF
+#            pass
+#
+##        if user["userid"] in required:
+##            print(f'+       {user}')
+##        elif user["userid"] in ignore:
+##            print(f'o       {user}')
+##        else:
+##            print(f'-       {user}')
 
 print()
 
@@ -248,10 +305,10 @@ print()
 # - Send an email summary to the KOA helpdesk with information for those programs that need their access updated 
 
 
-# construct email(s)
+# def construct email(s)
 
 
-# send email(s)
+# def send email(s)
 
 
 # if necessary to make make a class
